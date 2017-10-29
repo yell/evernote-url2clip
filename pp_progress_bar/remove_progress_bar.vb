64001a0a4b389e0dac472f24626be8e6baa909e4
monsta <yell.bondarenko@gmail.com>
2017-10-29 02:45:13 +0100

    remove progress bar functions

--- pp_progress_bar/remove_progress_bar.vb
+++
@@ -1,9 +0,0 @@
-Sub RemoveProgressBar()
-    On Error Resume Next
-        With ActivePresentation
-              For i = 1 To .Slides.Count
-              .Slides(i).Shapes("PBB").Delete
-              .Slides(i).Shapes("PBF").Delete
-              Next i:
-        End With
-End Sub
